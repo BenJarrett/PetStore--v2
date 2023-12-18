49dad35b14305d8c68d262e786652ba4d34d3331
BenCJ-INS <ben.jarrett@insight.com>
2023-12-18 06:52:59 -0500

    Add project files.

---
+++ PetStore-v2/Pets/Tiger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore_v2.Pets
+{
+    class Tiger
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public int NumberOfKills { get; set; }
+        public short Speed { get; set; }
+
+
+
+        // Fields // - For internal stuff
+        // - Things that are for me that I need in order to do my job. But don't neccesarily need other people to know about //
+        // - Used to stored data inside the class, but shouldn't be exposed externally to other people //
+        // - Things we want to keep internally // 
+        // Inplicitly private //
+        // Can be thought of as a class level variable //
+        // - Decalred almost exactly like a property // 
+        // - Info I need, but not info that I want to share //
+        // Naming fields is where one of the big differences are compared to Properties //
+        // Pre-fixed with an underscore\, then camelCased. Conventional idiomatic naming //
+        // No getter or setter. Almost works more like a variable //
+        // Fields are instance level by default, unless you specifiy that it is static //
+
+        // USE CASE OF STATIC FIELD //
+        /*        - static int _numberOfBites;
+                  - Then created 5 different Tigers and made them all bite people, we could use this to track how many bites there were across our entire population of Tigers */
+        // USE CASE OF STATIC FIELD //
+        // Fields //
+
+        int _numberOfBites;
+
+        public void Bite(string handlerName)
+        {
+            _numberOfBites++;
+            if (_numberOfBites > 5)
+            {
+                Console.WriteLine("This is a bitey Tiger");
+            }
+            Console.WriteLine($"{Name} the tiger just bit {handlerName}! That's the {_numberOfBites} time.");
+        }
+    }
+}
---
+++ PetStore-v2/Program.cs
@@ -0,0 +1,78 @@
+﻿using PetStore_v2.Pets; // Imports are at a namespace level // - order doesn't matter //
+using System;
+
+namespace PetStore_v2
+{
+    class Program
+    {
+        // Static Means that it shared across all instances and is additive.  Bc it doesn't have one of the four access modifiers, it's implicitly private. Static will take/call things like the 'Bark' method, and can call it on a specific instance of the class.
+        // Static members are shared across all of the instances. - what that means, is there is exactly one copy of the Bark() method that has to be shared accross all instances of the dog class. 
+        // That causes side effects. Can no longer access any properties that are instanced properties. 
+
+        // this methoid and property that is defined this way is known as an instance members // - Have to be called on instances of the class // 
+        // Static means shared across all instances //
+
+        // Private - implicitly private // - // Static - Not an instance method // - // Void - Doesn't return anything // - // Name - 'Main' // - // Param - T
+        static void Main(string[] args)
+        {
+            // Obstantiate new variable = to a new isntance of our class dog // - bc we set the class properties and bc they are publicly accessible, we are now able access/do stuff with them
+            // Now we can store this info on this instance of my Dog class //
+            var melba = new Dog("Melba", 60, "Medium");
+            melba.Bark();
+
+            // Won't work - property is privately set // 
+            // melba.Weight -= 15
+
+            Console.WriteLine($"What type of food should {melba.Name} eat?");
+            var typeOfFoodForMelba = Console.ReadLine();
+
+            melba.Eat(typeOfFoodForMelba);
+
+            // Won't work - Can't dynamically add properties to a class 
+            /*melba.CoatType = "Short"*/
+
+            var barley = new Dog("Barely", 50, "Medium");
+            barley.Bark();
+
+            barley.Eat("table scraps");
+
+            // Object Initializer // - if you create a class and setting properties immidiately after you create the class. Almost always it is idiomatic to do this. //
+            // Control . on the 3 dots under new. Changes the code a bit
+            // Another way to immidialtely set the properties on a class
+            // Not a constructor
+            var tiger = new Tiger
+            {
+                Name = "Kisa",
+                NumberOfKills = 0,
+                Size = "Medium",
+                Speed = 7,
+            };
+
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+            tiger.Bite("Chris");
+
+            var bear = new Bear("Yogi", "Brown");
+            // bear.NumberOfFishEaten = 5; // Won't work bc this property is { get; private set; } Have to go through the class to change this property. // 
+            // See the EatFish() method in the Bear class to see how to manipulate it //
+            bear.EatFish(5);
+            bear.EatFish(5);
+            bear.EatFish(5);
+            bear.EatFish(-5); // In this case, this what we don't want to allow. // In this case, we want rules around how many fish this can eat, and if we exposed it publicly, we would lose the control to create and manipulate these rules. In this case, we don't want this property to be able to move backwards.  // - The fish vomited 5 fish right back up // 
+            // We want people to knoiw how many fish this bear has eaten, but don't want to allow them to manipulate however they want. In this case they can only add to how many fish the bear has eaten. //
+            // The they in this case, is you in the future. //
+
+        }
+    }
+}
